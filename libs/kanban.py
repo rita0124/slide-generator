a891c648656c0a8de651e9c8518cedebc2566308
Rita <rita@nipapa.tw>
2023-09-10 11:20:46 +0800

    實作兩個方法與更新 config.ini.example

--- libs/kanban.py
+++ libs/kanban.py
@@ -12,9 +12,11 @@ class Kanban():
         config = configparser.ConfigParser()
         config.read('config.ini')
         configuration.access_token = config['Asana']['asana_token']
+        self.default = config['Default']
         # create an instance of the API class
         self.client = asana.ApiClient(configuration)
         self.workspaces = None
+        self.teams = None
         self.projects = None
         self.tasks = None
         self.get_all()
@@ -30,7 +32,41 @@ class Kanban():
         except ApiException as e:
             print("Exception when calling WorkspacesApi->get_workspaces: %s\n" % e)
 
+    def get_teams_in_workspace(self, workspace_gid=None):
+        try:
+            # create an instance of the API class
+            api_instance = asana.TeamsApi(self.client)
+            if workspace_gid is None:
+                # workspace_gid = self.workspaces[0]
+                workspace_gid = self.default['my_workspace_gid']
+            # Get multiple teams
+            api_response = api_instance.get_teams_for_workspace(workspace_gid)
+            data = api_response.to_dict()['data']
+            # self.teams = [ d['gid'] for d in data ]
+            self.teams = {}
+            for d in data:
+                self.teams[d['gid']] = d['name']
+        except ApiException as e:
+            print("Exception when calling TeamsApi->get_teams_for_workspace: %s\n" % e)
+
+    def get_projects_in_team(self, team_gid=None):
+        try:
+            # create an instance of the API class
+            api_instance = asana.ProjectsApi(self.client)
+            if team_gid is None:
+                team_gid = self.default['my_team_gid']
+            # Get multiple projects
+            api_response = api_instance.get_projects_for_team(team_gid)
+            data = api_response.to_dict()['data']
+            # self.projects = [ d['gid'] for d in data ]
+            self.projects = {}
+            for d in data:
+                self.projects[d['gid']] = d['name']
+        except ApiException as e:
+            print("Exception when calling ProjectsApi->get_projects_in_team: %s\n" % e)
+
     def get_all(self):
         self.get_workspaces()
-        # self.get_projects()
+        self.get_teams_in_workspace()
+        self.get_projects_in_team()
         # self.get_tasks()
